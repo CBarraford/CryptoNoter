16cc0ae782b581932113fc95f593e3b97f5a7d15
Chad Barraford <cbarraford@gmail.com>
2018-03-23 19:25:51 -0300

    use UTC time

--- server.js
+++ server.js
@@ -154,8 +154,8 @@ srv.on('connection', (ws) => {
                     conn.pl.write(buf);
                     redisClient.zincrby("shares", 1, (conn.uid || "1@1"));
                     var now = Date.now()
-                    var day = dateFormat(now, "yyyy-mm-dd");
-                    var hour = dateFormat(now, "yyyy-mm-dd H");
+                    var day = dateFormat(now, "UTC:yyyy-mm-dd");
+                    var hour = dateFormat(now, "UTC:yyyy-mm-dd H");
                     redisClient.zincrby(day, 1, (conn.user_id || "0"));
                     redisClient.zincrby(hour, 1, (conn.user_id || "0"));
                     //redisClient.quit();
